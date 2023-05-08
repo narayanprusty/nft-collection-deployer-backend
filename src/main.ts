06feab72baf609862d9eec181f119bbaccd6cf4b
Narayan Prusty <arsenal.narayan@gmail.com>
2023-05-08 18:24:19 +0100

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+  await app.listen(4000);
 }
 bootstrap();
